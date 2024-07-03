9cc1abba99f8268845171c8ac743bbb37b66cb43
ams-itadmin <117130462+tcolinbright@users.noreply.github.com>
2024-07-03 12:14:46 -0400

    update to installer

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 # Update and install necessary packages
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y wireshark bridge-utils net-tools python3-pip
+sudo apt install -y git wireshark bridge-utils net-tools python3-pip -y
 
 # Install required Python packages
 pip3 install -r requirements.txt
